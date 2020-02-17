e2492e95574c29cc3b690b46d8be073e8c39c956
Your Name <anish.king77@gmail.com>
2020-02-17 15:00:32 +0545

    added provider in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
+import 'package:epasal/provider/producs_provider.dart';
+import 'package:epasal/screens/product_details_screen.dart';
 import 'package:epasal/screens/product_overview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "E-pasal",
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: "E-pasal",
+        theme: ThemeData(
+            primarySwatch: Colors.green,
+            accentColor: Colors.red,
+            fontFamily: "MTCORSVA"),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductOverviewScreen.routeId: (context) => ProductOverviewScreen(),
+          ProductDetails.routeId: (context) => ProductDetails(),
+        },
       ),
-      home: ProductOverviewScreen(),
     );
   }
 }
