c15ce21293af8d28fffb334d0dfa382875791bb2
Your Name <anish.king77@gmail.com>
2020-02-23 15:05:26 +0545

    creatrd order provider and manage cart functions like dismissible

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,14 +1,18 @@
 import 'package:epasal/provider/cart_provider.dart' show Cart;
+import 'package:epasal/provider/order_provider.dart';
 import 'package:epasal/widgets/cart_item.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'order_screen.dart';
 
 class CartScreen extends StatelessWidget {
   static const String routeId = "/cart_screen";
 
   @override
   Widget build(BuildContext context) {
-    final cart = Provider.of<Cart>(context).items;
+    final cart = Provider.of<Cart>(context);
+    final order = Provider.of<Orders>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Cart"),
@@ -25,7 +29,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      "\$100",
+                      "\$${cart.totalAmount}",
                       style:
                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
@@ -39,7 +43,12 @@ class CartScreen extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           color: Theme.of(context).primaryColor),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      order.addOrder(
+                          cart.items.values.toList(), cart.totalAmount);
+                      cart.clearCart();
+                      Navigator.pushNamed(context, OrderScreen.routeId);
+                    },
                   ),
                 ],
               ),
@@ -50,7 +59,14 @@ class CartScreen extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-                itemCount: 3, itemBuilder: (context, i) => CartItem()),
+                itemCount: cart.itemCount,
+                itemBuilder: (context, i) => CartItem(
+                      id: cart.items.values.toList()[i].id,
+                      title: cart.items.values.toList()[i].title,
+                      productId: cart.items.keys.toList()[i],
+                      price: cart.items.values.toList()[i].price,
+                      quantity: cart.items.values.toList()[i].quantity,
+                    )),
           ),
         ],
       ),
